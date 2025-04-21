f06b4fbea252e7e91c48e3f216b046f11da87783
YC酱luyancib <2058282593@qq.com>
2025-04-21 20:53:55 +0800

    Update rsubot.sh

--- scripts/rsubot.sh
+++ scripts/rsubot.sh
@@ -13,6 +13,7 @@ file="$1"
 
 curl -s -F document=@$file "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
 	-F chat_id="$CHAT_ID" \
+	-F message_thread_id="$MESSAGE_THREAD_ID" \
 	-F "disable_web_page_preview=true" \
 	-F "parse_mode=markdownv2" \
 	-F caption="$msg"
